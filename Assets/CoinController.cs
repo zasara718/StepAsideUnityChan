2525b2e02318bc69875adff930b07580647a408d
zasara <zasara718@gmail.com>
2019-03-07 15:13:07 +0900

    Lesson6  課題
    課題の提出

---
+++ Assets/CoinController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//回転を開始する角度を設定
+		this.transform.Rotate (0, Random.Range (0, 360), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//回転
+		this.transform.Rotate (0, 3, 0);
+	}
+}
---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+	//ユニティちゃんのゲームオブジェクト型を宣言する
+	private GameObject maincamera;
+	// Use this for initialization
+	void Start () {
+		//ユニティちゃん変数にゲームオブジェクトを代入する
+		this.maincamera = GameObject.Find ("maincamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			//ユニティちゃんのZ座標より小さい場合オブジェクト破棄する
+			if (maincamera.transform.position.z > this.gameObject.transform.position.z) {
+				Destroy (this.gameObject);
+			}
+	}
+}
