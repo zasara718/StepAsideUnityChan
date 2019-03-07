e299dfb2b6dec8b190e519b17726e7ba2f28fd41
zasara <zasara718@gmail.com>
2019-03-07 18:36:00 +0900

    Lesson6 課題修正
    ユニティちゃんが通り過ぎたアイテムが消えるように修正しました。

--- Assets/ItemDestroy.cs
+++ Assets/ItemDestroy.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDestroy : MonoBehaviour {
-	//ユニティちゃんのゲームオブジェクト型を宣言する
+	//カメラのゲームオブジェクト型を宣言する
 	private GameObject maincamera;
+
 	// Use this for initialization
 	void Start () {
-		//ユニティちゃん変数にゲームオブジェクトを代入する
-		this.maincamera = GameObject.Find ("maincamera");
+		//カメラ変数にゲームオブジェクトを代入する
+		this.maincamera = GameObject.Find ("Main Camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			//ユニティちゃんのZ座標より小さい場合オブジェクト破棄する
+			//カメラのZ座標より小さい場合オブジェクト破棄する
 			if (maincamera.transform.position.z > this.gameObject.transform.position.z) {
 				Destroy (this.gameObject);
 			}
